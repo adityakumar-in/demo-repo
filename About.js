73a5aac694636bdad609c820d86a51a5c334a52e
Aditya Kumar <93466706+adityakumar-in@users.noreply.github.com>
2023-04-23 20:59:28 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,11 @@
+import React, { useRef } from 'react';
+
+
+export default function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This is my about page.</p>
+    </div>
+  )
+}
---
+++ Home.js
@@ -0,0 +1,11 @@
+import React, { useRef } from 'react';
+
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>Welcome to my home page!</p>
+    </div>
+  )
+}
